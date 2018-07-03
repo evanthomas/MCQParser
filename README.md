7d4a9ac1d1b5a797d4cae611e74ef0cdc4f10e99
Evan Thomas <thomas.e@wehi.edu.au>
2018-07-03 15:28:34 +1000

    Update to bold correct answer

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MCQParser
 A simple tool for extracting multiple choice questions from a Backboard content package.
 
-You will need to pip install zipfile, beautifulsoup4 and html5lib. Then just pass the zip file, no need to uncompress, 
+You will need to pip install xmltodict, zipfile, beautifulsoup4 and html5lib. Then just pass the zip file, no need to uncompress, 
 and an html file is produced.  Obviously, I can produce something in another format if you like, e.g. markdown. For some
 reason, some the text is wrapped in HTML and some isn’t. I’ve stripped any HTML wrapping.
--- mcq_parse.py
+++ mcq_parse.py
@@ -4,16 +4,34 @@ import os
 import zipfile
 from bs4 import BeautifulSoup
 
+class Choice:
+  def __init__(self, choice_text, id):
+    self.choice_text = choice_text
+    self.id = id
+
+  def is_correct(self, id):
+    return id == self.id
+
 class MCQ:
-  def __init__(self, presentation):
-    flows = presentation['flow']['flow']
-    
+  def __init__(self, item):
+
+    flows = item['presentation']['flow']['flow']
+
     self.answers = []
     for flow in flows:
       if flow['@class'] == 'QUESTION_BLOCK':
         self.question = self._extractQuestion(flow)
       if flow['@class'] == 'RESPONSE_BLOCK':
-        self.answers = self._extractAnswers(flow)
+        self.choices = self._extractChoices(flow)
+
+    self.correct_choice_id = None
+    response_conditions = item['resprocessing']['respcondition']
+    for response_condition in response_conditions:
+      try:
+        if response_condition['@title'] == 'correct':
+          self.correct_choice_id = response_condition['conditionvar']['varequal']['#text']
+      except KeyError:
+        pass
 
   def _extractQuestion(self, xml):
     try:
@@ -21,21 +39,25 @@ class MCQ:
     except KeyError:
       raise Exception('Question not in expected format')
 
-  def _extractAnswers(self, xml):
-    answers = []
+  def _extractChoices(self, xml):
+    choices = []
     try:
-      answersXml = xml['response_lid']['render_choice']['flow_label']
-      for answer in answersXml:
-        at = self._clean(answer['response_label']['flow_mat']['material']['mat_extension']['mat_formattedtext'])
-        answers.append(at)
+      choicesXml = xml['response_lid']['render_choice']['flow_label']
+      for c in choicesXml:
+        at = self._clean(c['response_label']['flow_mat']['material']['mat_extension']['mat_formattedtext'])
+        id = c['response_label']['@ident']
+        choices.append(Choice(at, id))
     except KeyError:
-      raise Exception('Answer not in expected format')
+      raise Exception('Choice not in expected format')
 
-    return answers
+    return choices
+
+  def _extractAnswers(self, xml):
+    pass
 
   def _clean(self, xml):
     text = xml['#text']
-    formatting = xml['@type'] 
+    formatting = xml['@type']
     if formatting == 'HTML':
       soup = BeautifulSoup(text, "html5lib")
       text = soup.get_text()
@@ -44,15 +66,23 @@ class MCQ:
   def __str__(self):
     s = self.question + '\n'
     n = 1
-    for a in self.answers:
-      s = s + '   ' + str(n) + '. ' + a + '\n'
+    for a in self.choiced:
+      if a.is_correct(self.correct_choice_id):
+        ast = '* '
+      else:
+        ast = ''
+      s = s + '   ' + str(n) + '. ' + ast + a.choice_text + '\n'
       n = n + 1
     return s
 
   def toHTML(self):
     s = '<p>' + self.question + '\n<ol>'
-    for answer in self.answers:
-      s = s + '<li>' + answer + '</li>\n'
+    for choice in self.choices:
+      if choice.is_correct(self.correct_choice_id):
+        tt = '<b>' + choice.choice_text + '</b>'
+      else:
+        tt = choice.choice_text
+      s = s + '<li>' + tt + '</li>\n'
     s = s + '</ol>\n</p><br>\n'
     return s
 
@@ -76,8 +106,7 @@ class Resource:
     self.mcqs = []
     for item in items:
       try:
-        presentation = item['presentation']
-        self.mcqs.append(MCQ(presentation))
+        self.mcqs.append(MCQ(item))
       except KeyError:
         pass
 
@@ -117,7 +146,7 @@ def main():
     html = html + resource.toHTML()
   html = html + '</body>\n</html>'
 
-  htmlFile = os.path.basename(inf) + '.html'
+  htmlFile = os.path.join(os.path.dirname(inf), os.path.basename(inf) + '.html')
   with open(htmlFile, 'w') as f:
     f.write(html)
 
